3998c54a8117d9579278d4d3e0b1f94c214145fa
sanyajain24 <sanyajain2403@gmail.com>
2019-07-26 11:29:01 -0700

    variables and ifs

---
+++ convo.py
@@ -0,0 +1,21 @@
+# run with python3 convo.py in terminal
+name = input("What is your name?")
+print("Nice to meet you,",name)
+
+color = input("What is your favorite color? ")
+print(color, "is a great choice!")
+
+flavor = input("What is your favorite ice cream flavor? ")
+if(flavor == "vanilla"):
+    print("I like",flavor, "as well!")
+else:
+    print("ew that's a bad flavor")
+
+
+
+
+
+
+
+
+
---
+++ variables.py
@@ -0,0 +1,13 @@
+# variables are used to store some sort of value in a saved place
+x = input("What is x? ")
+y = input("What is y? ")
+
+#your code here to swap the variable values of x and y
+temp = x
+x = y
+y = temp
+
+#printing after swapping variables
+print("I swapped x and y, look:")
+print("x is now ",x)
+print("y is now ",y)
